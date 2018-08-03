0f0245c3f8e1f532ce5ef689fda2d3bc67c1e90f
Vicki Hamm <vicki.cello@gmail.com>
2018-08-03 18:54:33 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   "SELECT MAX(gpa) FROM students;"
@@ -16,5 +16,3 @@ end
 
 def average_gpa_for_9th_grade
 end
-
-
